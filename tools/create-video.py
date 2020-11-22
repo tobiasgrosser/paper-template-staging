a6342a1fc0c83efa618628b119a3511b06bcff22
tobiasgrosser <tobiasgrosser@users.noreply.github.com>
2020-11-22 15:31:59 +0000

    deploy: 51189b9639a809a3b559bf16294b7eec976d42f4

--- tools/create-video.py
+++ tools/create-video.py
@@ -13,24 +13,27 @@ def getPageNumber(name):
     files = list(files)
     return len(files)
 
-def removePNGFiles(directory):
+def removePNGFiles(directory, tmpOnly = False):
     files = os.listdir(directory)
     files = filter(lambda x: x.find(".png") != -1, files)
+    if tmpOnly:
+        files = filter(lambda x: x.find("-tmp") != -1, files)
+
     for f in files:
       os.remove(directory + '/' + f)
 
-def createImageOfPaper(path, width = 6, pages = 18):
+def createImageOfPaper(path, width = 7, pages = 21):
     print(path)
     removePNGFiles(os.path.dirname(path))
 
     name = path
 
-    cmd = ['convert', '-density', '300', path, '-resize', '40%', '-background', 'white', '-alpha', 'remove', '-alpha', 'off', name + ".png"]
+    cmd = ['convert', '-density', '300', path, '-resize', '40%', '-background', 'white', '-alpha', 'remove', '-alpha', 'off', name + "-tmp.png"]
     run(cmd)
 
     pageNumber = getPageNumber(path);
     if pageNumber == 1:
-        shutil.copyfile(name + ".png", name + "-0.png")
+        shutil.copyfile(name + ".png", name + "-tmp-0.png")
 
     for i in range(pageNumber, pages):
         cmd = ['convert', '-size', '1020x1320', 'xc:white', name + '-' + str(i) + '.png']
@@ -41,14 +44,15 @@ def createImageOfPaper(path, width = 6, pages = 18):
         for imageId in range(row * width, min((row + 1) * width, pages)):
             cmd.append(name + "-" + str(imageId) + ".png")
         cmd.append("+append")
-        cmd.append(name + "-row-" + str(row) + ".png")
+        cmd.append(name + "-tmp-row-" + str(row) + ".png")
         run(cmd)
 
     cmd = ['convert']
     for row in range(0, math.ceil(pages / width)):
-      cmd.append(name + "-row-" + str(row) + ".png")
+      cmd.append(name + "-tmp-row-" + str(row) + ".png")
     cmd.append("-append")
     cmd.append(name + "-full.png")
+    removePNGFiles(os.path.dirname(path), tmpOnly=True)
     run(cmd)
 
 def getReleases(path):
@@ -84,7 +88,7 @@ def moveImages(path):
 
 def createVideo(path):
     run(['rm', path + 'video.mp4'])
-    cmd = ['ffmpeg', '-r', '1', '-i', path + 'still-%05d.png', '-vcodec', 'libx264', '-crf', '25', '-pix_fmt', 'yuv420p', path + 'video.mp4']
+    cmd = ['ffmpeg', '-r', '1', '-i', path + 'still-%05d.png', '-vcodec', 'libx264', '-crf', '25', '-pix_fmt', 'yuv420p', '-vf', 'scale=3840:2160', path + 'video.mp4']
     run(cmd)
     print(path + 'video.mp4')
 
